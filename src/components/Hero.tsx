bc5060b7fcfe1de58d167dacfa351745293a188a
Muhammad Asaad Aslam <muhammadasaad30@gmail.com>
2024-10-05 23:17:23 +0500

    Add files via upload

---
+++ src/components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+const main = () => {
+  return (
+    <div id="main" className='min-h-screen bg-no-repeat bg-[url(/profile.jpg)] bg-Right lg:bg-[75%] bg-cover'
+    style={{backgroundSize: "15%"}} >
+   
+    <Navbar />
+
+    </div>
+  )
+}
+
+export default main
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='container pt-8'>
+        <div className='flex justify-between items-center'>
+        <div className='text-xl font-medium' style={{marginTop: "25%"}}>Hello I am Muhammad Asaad</div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
